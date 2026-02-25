365e7ae20f209922e7a489bebeba01de687913d3
andytwoods <andytwoods@gmail.com>
2026-02-25 19:54:45 +0000

    Fix typo in `HomepageFeatures` text ("hadnt" to "

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -5,7 +5,7 @@ export default function HomepageFeatures(): React.JSX.Element {
     <section>
       <p>This idea is super easy. It's quick to set up, and it gets people talking. Find a space in your department, your shared kitchen space, or even in the corridor, and start populating it with books on inclusion.</p>
       <p>We started doing this a few years ago. We were kindly given £100 by our group to ebay some books (see our list below) and a colleague's partner kindly built a shelf. The books started arriving in the post and our shelf took off!</p>
-        <p>And new books starting appearing, that we hadnt bought. As well as inclusion-based items, such as a flyer for "<a href='https://andysmanclub.co.uk/' target='_blank' rel='noopener noreferrer'>Andy's Man Club<span className="sr-only"> (opens in new tab)</span></a>".</p>
+        <p>And new books starting appearing, that we hadn't bought. As well as inclusion-based items, such as a flyer for "<a href='https://andysmanclub.co.uk/' target='_blank' rel='noopener noreferrer'>Andy's Man Club<span className="sr-only"> (opens in new tab)</span></a>".</p>
       <p>The shelf grew so popular we got another £150 to buy more books.</p>
       <p>With chatted with friends around campus about the bookshelf. A few months later other bookshelves starting popping up in different departments.</p>
       <p>Is this the start of a plague of inclusive bookshelves? We hope so!</p>
