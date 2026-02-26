aab5b15d5ece4fc77e4dbfe7966a503e16edf1b4
andytwoods <andytwoods@gmail.com>
2026-02-26 19:20:57 +0000

    Refine text in "Why this is a good idea" list for tone adjustment.

--- src/components/WhyGoodIdea/index.tsx
+++ src/components/WhyGoodIdea/index.tsx
@@ -34,7 +34,7 @@ export default function WhyGoodIdea(): React.JSX.Element {
         <li>Lives in shared spaces, so is noticed</li>
         <li>Sparks conversations and recommendations</li>
         <li>Grows organically through donations and interest</li>
-        <li>Looks good when guests come around (!)</li>
+        <li>Looks good when guests come around (ugh)</li>
         <li>There is pride about the bookshelf</li>
         <li>Self replicants, von-Neumann style</li>
         <li>We make the world a tiny tiny bit better</li>
